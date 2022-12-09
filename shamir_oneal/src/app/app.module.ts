eb184b006cac31697c2415d85e62b0541c0dc62d
itzBaxter009 <pedropardog009@gmail.com>
2022-12-09 11:54:03 -0600

    SignUp page completed

--- shamir_oneal/src/app/app.module.ts
+++ shamir_oneal/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -8,10 +8,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input'
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes =[
+  { path: '', component:LoginComponent},
+  { path: 'signup', component:SignupComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
@@ -19,9 +27,11 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatCheckboxModule,
-    MatInputModule  
+    MatInputModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports:[RouterModule]
 })
 export class AppModule { }
